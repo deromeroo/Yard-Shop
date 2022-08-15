79b7e2737b7315e0c8575a55fc3d951bd067ae70
Diego Romero <deromero707@gmail.com>
2022-08-15 18:56:55 -0400

    [FIX] Detail in img tag

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -17,7 +17,7 @@ const ProductItem = ({product}) => {
 
 	return (
 		<div className="ProductItem">
-			{/* <img src={product.images[0]} alt={product.title}/> */}
+			<img src={product.images[0]} alt={product.title}/>
 			<div className="product-info">
 				<div>
 					<p>${product.price}</p>
